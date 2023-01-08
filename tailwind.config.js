4591d97dbd63b022031d90c2007dfa1b9181e113
herlandi <herlandi619@gmail.com>
2023-01-08 15:43:52 +0700

    tambah fitur darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
+  darkMode: 'class',
   theme: {
       container: {
         center: true,
@@ -11,6 +12,8 @@ module.exports = {
         primary:'#22d3ee',
         dark:'#0f172a',
         secondary:'#64748b',
+        dark2:'#334155',
+        tl:'#e2e8f0',
       },
       screen:{
         '2xl':'1320px',
